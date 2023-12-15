40b979f23feef654ccbad1d1ab693b8e2eae9a5f
Hendrik Schuster <hendrik.schuster@consileon.de>
2023-12-15 09:21:18 +0100

    xml configuration

--- src/main/java/platform/codingnomads/co/corespring/examples/application_context/xml/CodingNomad.java
+++ src/main/java/platform/codingnomads/co/corespring/examples/application_context/xml/CodingNomad.java
@@ -1,7 +1,6 @@
 package platform.codingnomads.co.corespring.examples.application_context.xml;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
@@ -12,17 +11,20 @@ public class CodingNomad {
     private final JDK jdk;
     private final IDE ide;
     private final Framework framework;
+    private final Browser browser;
 
     public String createAwesomeSoftware() {
         return MessageFormat
                 .format("This coding nomad is creating awesome software using, " +
-                                "IDE:({0}:{1}), JDK: ({2}:{3}), Framework:({4}:{5})",
+                                "IDE:({0}:{1}), JDK: ({2}:{3}), Framework:({4}:{5}), Browser:({6}:{7})",
                         ide.getName(),
                         ide.getVersion(),
                         jdk.getName(),
                         jdk.getVersion(),
                         framework.getName(),
-                        framework.getVersion()
+                        framework.getVersion(),
+                        browser.getName(),
+                        browser.getVersion()
                 );
     }
 }
