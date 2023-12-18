b2bac9ab439189d6d2c71bb33515dbb57cd34485
Hendrik Schuster <hendrik.schuster@consileon.de>
2023-12-18 15:50:56 +0100

    Labs: Spring Core

--- src/main/java/platform/codingnomads/co/corespring/lab/practice/BandConfig.java
+++ src/main/java/platform/codingnomads/co/corespring/lab/practice/BandConfig.java
@@ -8,9 +8,6 @@ import org.springframework.context.annotation.ImportResource;
 @Configuration
 @ImportResource({"classpath*:xml-config/musicians.xml"})
 public class BandConfig {
-
-    @Bean
-    public Band band() { return new Band(); }
     @Bean
     public Singer singer() { return new Singer("Ronnie James Dio"); }
 
