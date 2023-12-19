57d9e36dca593896fba97afdde12315d8780b0d9
Hendrik Schuster <hendrik.schuster@consileon.de>
2023-12-19 09:13:47 +0100

    using spring data jdbc

--- src/main/java/platform/codingnomads/co/corespring/lab/practice/Band.java
+++ src/main/java/platform/codingnomads/co/corespring/lab/practice/Band.java
@@ -2,14 +2,18 @@ package platform.codingnomads.co.corespring.lab.practice;
 
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @ToString
 @NoArgsConstructor
 public class Band {
+    @Autowired
     private Singer singer;
+    @Autowired
     private Guitarist guitarist;
+    @Autowired
     private Drummer drummer;
 
     public Band(Singer singer, Guitarist guitarist, Drummer drummer) {
--- src/main/java/platform/codingnomads/co/springdata/example/springdatajdbc/SpringDataJDBCDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/springdatajdbc/SpringDataJDBCDemo.java
@@ -31,14 +31,30 @@ public class SpringDataJDBCDemo implements CommandLineRunner {
             //nothing
         }
 
+        try {
+            //create employee table using the JdbcTemplate method "execute"
+            jdbcTemplate.execute("CREATE TABLE nfl_players (id BIGINT AUTO_INCREMENT PRIMARY KEY," +
+                    "first_name VARCHAR(255) NOT NULL,last_name  VARCHAR(255) NOT NULL);");
+        } catch (Exception e) {
+            //nothing
+        }
+
         //create a list of first and last names
         List<Object[]> splitUpNames = Stream.of("Java Ninja", "Spring Guru", "Java Guru", "Spring Ninja")
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        List<Object[]> splitUpPlayers = Stream.of("Tom Brady", "Jerome Bettis", "Odell Beckham", "Christian McCaffrey")
                 .map(name -> name.split(" "))
                 .collect(Collectors.toList());
 
         //for each first & last name pair insert an Employee into the database
         for (Object[] name : splitUpNames) {
             jdbcTemplate.execute(String.format("INSERT INTO employees(first_name, last_name) VALUES ('%s','%s')", name[0], name[1]));
+        }
+
+        for (Object[] player : splitUpPlayers) {
+            jdbcTemplate.execute(String.format("INSERT INTO nfl_players(first_name, last_name) VALUES ('%s','%s')", player[0], player[1]));
         }
 
         //query the database for Employees with first name Java
@@ -49,9 +65,22 @@ public class SpringDataJDBCDemo implements CommandLineRunner {
                 //print each found employee to the console
                 .forEach(employee -> System.out.println(employee.toString()));
 
+        jdbcTemplate.query(
+                        "SELECT id, first_name, last_name FROM nfl_players WHERE first_name = 'Christian'",
+                        (rs, rowNum) -> {
+                            return new NFLPlayer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"));
+                        }
+                )
+                //print each found employee to the console
+                .forEach(player -> System.out.println(player.toString()));
+
         //truncate the table
         jdbcTemplate.execute("TRUNCATE TABLE employees;");
         //delete the table
         jdbcTemplate.execute("DROP TABLE employees");
+
+        jdbcTemplate.execute("TRUNCATE TABLE nfl_players;");
+        //delete the table
+        jdbcTemplate.execute("DROP TABLE nfl_players");
     }
 }
