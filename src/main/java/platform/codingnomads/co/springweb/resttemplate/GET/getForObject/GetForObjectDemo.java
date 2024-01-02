7f5d59853ffc794d0c0c15f96f7c5a8ee11d6830
Hendrik Schuster <hendrik.schuster@consileon.de>
2024-01-02 17:22:21 +0100

    spring web GET map

--- src/main/java/platform/codingnomads/co/springweb/resttemplate/GET/getForObject/GetForObjectDemo.java
+++ src/main/java/platform/codingnomads/co/springweb/resttemplate/GET/getForObject/GetForObjectDemo.java
@@ -5,12 +5,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import platform.codingnomads.co.springweb.resttemplate.GET.models.ActivityTemplate;
 import platform.codingnomads.co.springweb.resttemplate.GET.models.ExcuseTemplate;
 import platform.codingnomads.co.springweb.resttemplate.GET.models.QuoteTemplate;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class GetForObjectDemo {
@@ -41,6 +46,18 @@ public class GetForObjectDemo {
             ExcuseTemplate[] randomExcuse;
             randomExcuse = restTemplate.getForObject("https://excuser-three.vercel.app/v1/excuse/3", ExcuseTemplate[].class);
             System.out.println(Arrays.toString(randomExcuse));
+
+            Map<String, Object> params = new HashMap<>();
+            params.put("type", "social");
+
+            ResponseEntity<ActivityTemplate[]> responseEntity =
+                    restTemplate.getForEntity("https://www.boredapi.com/api/activity", ActivityTemplate[].class, params);
+            if (responseEntity.getStatusCode().equals(HttpStatus.OK) && responseEntity.getBody() != null) {
+                ActivityTemplate[] activityTemplate = responseEntity.getBody();
+                System.out.println(Arrays.toString(activityTemplate));
+            } else {
+                System.out.println("Something went wrong! The response was not marked with status code 200");
+            }
 
         };
     }
---
+++ src/main/java/platform/codingnomads/co/springweb/resttemplate/GET/models/ActivityTemplate.java
@@ -0,0 +1,14 @@
+package platform.codingnomads.co.springweb.resttemplate.GET.models;
+
+import lombok.Data;
+
+@Data
+public class ActivityTemplate {
+    private String activity;
+    private String accessibility;
+    private String type;
+    private String participants;
+    private String price;
+    private String link;
+    private String key;
+}
