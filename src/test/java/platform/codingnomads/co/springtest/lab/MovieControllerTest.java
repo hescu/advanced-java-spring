5b874305511220d6eb0bc04e3abeb915b1e6a78c
Hendrik Schuster <hendrik.schuster@consileon.de>
2024-01-25 15:40:09 +0100

    task 1

--- src/test/java/platform/codingnomads/co/springtest/lab/MovieControllerTest.java
+++ src/test/java/platform/codingnomads/co/springtest/lab/MovieControllerTest.java
@@ -1,31 +1,49 @@
-//package platform.codingnomads.co.springtest.lab;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.web.servlet.MockMvc;
-//import platform.codingnomads.co.springtest.lab.SpringTestLab;
-//
-//@SpringBootTest(classes = SpringTestLab.class)
-//@AutoConfigureMockMvc
-//public class MovieControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Test
-//    public void testGetAllMoviesSuccess() {
-//
-//    }
-//
-//    @Test
-//    public void testGetAllMoviesFailure() {
-//
-//    }
-//
-//    @Test
-//    public void testGetAllMoviesSuccessMockService() {
-//
-//    }
-//}
+package platform.codingnomads.co.springtest.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import platform.codingnomads.co.springtest.lab.SpringTestLab;
+import platform.codingnomads.co.springtest.lab.repository.MovieRepository;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(classes = SpringTestLab.class)
+@AutoConfigureMockMvc
+public class MovieControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MovieRepository movieRepo;
+
+    @Test
+    public void testGetAllMoviesSuccess() throws Exception {
+        mockMvc.perform(get("/all"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void testGetAllMoviesFailure() throws Exception {
+        movieRepo.deleteAll();
+
+        mockMvc.perform(get("/all"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void testGetAllMoviesSuccessMockService() {
+
+    }
+}
